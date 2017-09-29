5343ab2052699200f270a1aa5bffb71483817165
shantanu2 <shantanuscriet@gmail.com>
2017-09-29 10:36:38 +0530

    SocialNet
    Version 0.1

---
+++ js/project.js
@@ -0,0 +1,16 @@
+var $document = $(document),
+    $element =$('.navbar'),
+    navbarDefault='navbar-default';
+    navbarTransparent ='navbar-transparent';
+    fadeInDown = 'fadeInDown';
+ $document.scroll(function(){
+     if($document.scrollTop() >= 100){
+         $element.addClass(navbarDefault);
+         $element.removeClass(navbarTransparent);
+         $element.addClass(fadeInDown);
+     } else{
+         $element.addClass(navbarTransparent);
+         $element.removeClass(navbarDefault);
+         $element.removeClass(fadeInDown);    
+     }   
+ });
